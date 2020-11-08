53c9ea9a850e71d968b6f9c4cee8201b83c65dcd
Gabriel Westmarwick <westgabr@amazon.com>
2020-11-08 20:24:01 +0000

    add more meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,10 @@ export default function Index() {
             <meta property="og:title" content="Leith Theatre's Big Radiator Generator"></meta>
             <meta property="og:description" content="Help Leith Theatre protect its structure and its future whilst spreading warmth to its community this winter"></meta>
             <meta property="og:image" content="https://crowd-temp.vercel.app/api/temp"></meta>
+            <meta property="og:image:width" content="300"></meta>
+            <meta property="og:image:height" content="300"></meta>
             <meta property="og:url" content="https://www.crowdfunder.co.uk/leith-theatres-big-radiator-generator"></meta>
+            <meta property="og:type" content="website"></meta>
             <meta name="twitter:card" content="summary_large_image"></meta>
         </Head>
     )
