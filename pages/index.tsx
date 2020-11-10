8469b02b043c8cba6801a261db5c1d61abb2201c
Gabriel Westmarwick <westgabr@amazon.com>
2020-11-10 10:16:13 +0000

    try cache busting query param

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
             <title></title>
             <meta property="og:title" content="Leith Theatre's Big Radiator Generator"></meta>
             <meta property="og:description" content="Help Leith Theatre protect its structure and its future whilst spreading warmth to its community this winter"></meta>
-            <meta property="og:image" content="https://crowd-temp.vercel.app/api/temp/leith-theatres-big-radiator-generator/light/static/1146x600"></meta>
+            <meta property="og:image" content={`https://crowd-temp.vercel.app/api/temp/leith-theatres-big-radiator-generator/light/static/1146x600?${Math.random().toString().split('.')[1]}`}></meta>
             <meta property="og:image:width" content="1146"></meta>
             <meta property="og:image:height" content="600"></meta>
             <meta property="og:url" content="https://www.crowdfunder.co.uk/leith-theatres-big-radiator-generator"></meta>
