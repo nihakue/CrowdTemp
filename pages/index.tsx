968834eced1300441b28a22999b4c987bde5222d
Gabriel Westmarwick <westgabr@amazon.com>
2020-11-11 13:36:48 +0000

    make meta ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
 
-export default function Index() {
+export default function Index({rand}) {
     useEffect(() => {
         if (window) {
             window.location.href="https://www.crowdfunder.co.uk/leith-theatres-big-radiator-generator";
@@ -12,7 +13,7 @@ export default function Index() {
             <title></title>
             <meta property="og:title" content="Leith Theatre's Big Radiator Generator"></meta>
             <meta property="og:description" content="Help Leith Theatre protect its structure and its future whilst spreading warmth to its community this winter"></meta>
-            <meta property="og:image" content={`https://crowd-temp.vercel.app/api/temp/leith-theatres-big-radiator-generator/light/static/1146x600?${Math.random().toString().split('.')[1]}`}></meta>
+            <meta property="og:image" content={`https://crowd-temp.vercel.app/api/temp/leith-theatres-big-radiator-generator/light/static/1146x600?${rand}`}></meta>
             <meta property="og:image:width" content="1146"></meta>
             <meta property="og:image:height" content="600"></meta>
             <meta property="og:url" content="https://www.crowdfunder.co.uk/leith-theatres-big-radiator-generator"></meta>
@@ -20,4 +21,12 @@ export default function Index() {
             <meta name="twitter:card" content="summary_large_image"></meta>
         </Head>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    return {
+        props: {
+            rand: Math.random().toString().split('.')[1]
+        },
+    }
 }
